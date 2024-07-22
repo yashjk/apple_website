d03c4409b2a974884fb31929be4f10431577fbb8
yash <injose.joshi@gmail.com>
2024-07-22 16:41:00 +0530

    Add How it works section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import * as Sentry from "@sentry/react";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 			<Highlights />
 			<Model />
       <Features />
+      <HowItWorks />
 		</main>
 	);
 };
